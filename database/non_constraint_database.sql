e230f598884b252f253e5731c0104dd3719ccf11
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-02-23 11:34:32 +0700

    Update non-constraint database

--- database/non_constraint_database.sql
+++ database/non_constraint_database.sql
@@ -1,212 +1,204 @@
-Enum "orders_status_of_order_enum" {
-  "Pending"
-  "Processing"
-  "Shipped"
-  "Delivered"
-  "Cancelled"
-}
+CREATE TABLE `accounts` (
+  `account_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `created_at` datetime DEFAULT (now()),
+  `updated_at` datetime,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "accounts" {
-  "account_id" int(11) [unique, pk, not null, increment]
-  "role_id" int(11) [not null]
-  "username" varchar(200) [not null]
-  "password" varchar(300) [not null]
-  "created_at" datetime [default: `now()`]
-  "updated_at" datetime
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `brands` (
+  `brand_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `brand_name` varchar(100) DEFAULT '' COMMENT 'Ex: SANYO, TOSHIBA,...',
+  `brand_logo` varchar(300) DEFAULT '',
+  `supplier_id` int(11) NOT NULL,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "brands" {
-  "brand_id" int(11) [unique, pk, not null, increment]
-  "brand_name" varchar(100) [default: "", note: "Ex: SANYO, TOSHIBA,..."]
-  "brand_logo" varchar(300) [default: ""]
-  "supplier_id" int(11) [not null]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `categories` (
+  `categories_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `categories_name` varchar(100) DEFAULT '' COMMENT 'Ex: Tủ lạnh, máy giặt,...',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "categories" {
-  "categories_id" int(11) [unique, pk, not null, increment]
-  "categories_name" varchar(100) [default: "", note: "Ex: Tủ lạnh, máy giặt,..."]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `customers` (
+  `customer_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `customer_fullname` varchar(100) DEFAULT '',
+  `role_id` int(11) NOT NULL,
+  `gender` bit(1) DEFAULT 0 COMMENT 'Male: 0, Female: 1',
+  `phone_number` varchar(20) DEFAULT '',
+  `customer_email` varchar(200) DEFAULT '',
+  `address` varchar(200) DEFAULT '' COMMENT 'Địa chỉ của khách hàng',
+  `date_of_birth` date,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "customers" {
-  "customer_id" int(11) [unique, pk, not null, increment]
-  "customer_fullname" varchar(100) [default: ""]
-  "role_id" int(11) [not null]
-  "gender" bit(1) [default: 0, note: "Male: 0, Female: 1"]
-  "phone_number" varchar(20) [default: ""]
-  "customer_email" varchar(200) [default: ""]
-  "address" varchar(200) [default: "", note: "Địa chỉ của khách hàng"]
-  "date_of_birth" date
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `decentralizations` (
+  `decentralization_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `function_id` int(11) NOT NULL,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "decentralizations" {
-  "decentralization_id" int(11) [unique, pk, not null, increment]
-  "role_id" int(11) [not null]
-  "module_id" int(11) [not null]
-  "function_id" int(11) [not null]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `exports` (
+  `export_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `staff_id` int(11) NOT NULL,
+  `export_date` datetime DEFAULT (now()),
+  `unit_price_export` decimal(10,2) DEFAULT 0,
+  `quantity_export` int(50) DEFAULT 0,
+  `total_price` decimal(10,2) DEFAULT 0 COMMENT 'Không tự sinh đc như mysql',
+  `reason_id` int(11) NOT NULL COMMENT 'bán - customer, trả - supllier',
+  `order_detail_id` int(11) NOT NULL,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "exports" {
-  "export_id" int(11) [unique, pk, not null, increment]
-  "staff_id" int(11) [not null]
-  "export_date" datetime [default: `now()`]
-  "unit_price_export" decimal(10,2) [default: 0]
-  "quantity_export" int(50) [default: 0]
-  "total_price" decimal(10,2) [default: 0, note: "Không tự sinh đc như mysql"]
-  "reason_id" int(11) [not null, note: "bán - customer, trả - supllier"]
-  "order_detail_id" int(11) [not null]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `functions` (
+  `function_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `function_name` varchar(100) DEFAULT '',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "functions" {
-  "function_id" int(11) [unique, pk, not null, increment]
-  "function_name" varchar(100) [default: ""]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `imports` (
+  `import_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `staff_id` int(11) NOT NULL,
+  `shipment_id` int(11) NOT NULL,
+  `reason_id` int(11) NOT NULL,
+  `import_date` datetime DEFAULT (now()),
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "imports" {
-  "import_id" int(11) [unique, pk, not null, increment]
-  "staff_id" int(11) [not null]
-  "shipment_id" int(11) [not null]
-  "reason_id" int(11) [not null]
-  "import_date" datetime [default: `now()`]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `modules` (
+  `module_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `module_name` varchar(200) DEFAULT '',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "modules" {
-  "module_id" int(11) [unique, pk, not null, increment]
-  "module_name" varchar(200) [default: ""]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `orders` (
+  `order_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `receiver_name` varchar(100) DEFAULT '' COMMENT 'Có thể giấu tên',
+  `email_of_receiver` varchar(100) NOT NULL,
+  `phone_number_of_receiver` varchar(20) NOT NULL,
+  `address_of_receiver` varchar(200) NOT NULL,
+  `note` varchar(100) DEFAULT '',
+  `order_date` datetime DEFAULT (now()),
+  `status_of_order` ENUM ('Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled') DEFAULT 'Pending',
+  `total_money` decimal(10,2) DEFAULT 0,
+  `shipping_method` varchar(100) DEFAULT '',
+  `shipping_address` varchar(200) DEFAULT '',
+  `shipping_date` datetime,
+  `tracking_number` varchar(100) DEFAULT '',
+  `payment_method` varchar(100) DEFAULT '',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "orders" {
-  "order_id" int(11) [unique, pk, not null, increment]
-  "account_id" int(11) [not null]
-  "receiver_name" varchar(100) [default: "", note: "Có thể giấu tên"]
-  "email_of_receiver" varchar(100) [not null]
-  "phone_number_of_receiver" varchar(20) [not null]
-  "address_of_receiver" varchar(200) [not null]
-  "note" varchar(100) [default: ""]
-  "order_date" datetime [default: `now()`]
-  "status_of_order" orders_status_of_order_enum [default: "Pending"]
-  "total_money" decimal(10,2) [default: 0]
-  "shipping_method" varchar(100) [default: ""]
-  "shipping_address" varchar(200) [default: ""]
-  "shipping_date" datetime
-  "tracking_number" varchar(100) [default: ""]
-  "payment_method" varchar(100) [default: ""]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `order_details` (
+  `order_detail_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` decimal(10,2) DEFAULT 0,
+  `number_of_products` int(11) DEFAULT 1 COMMENT 'Phải > 0',
+  `total_money` float DEFAULT 0 COMMENT 'Phải >= 0',
+  `color_of_product` varchar(20) DEFAULT '',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "order_details" {
-  "order_detail_id" int(11) [unique, pk, not null, increment]
-  "order_id" int(11) [not null]
-  "product_id" int(11) [not null]
-  "price" decimal(10,2) [default: 0]
-  "number_of_products" int(11) [default: 1, note: "Phải > 0"]
-  "total_money" float [default: 0, note: "Phải >= 0"]
-  "color_of_product" varchar(20) [default: ""]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `products` (
+  `product_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(350) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `price` decimal(10,2) DEFAULT 0 COMMENT 'Phải >= 0',
+  `thumbnail` varchar(300) DEFAULT '' COMMENT 'Phải có ảnh mặc định',
+  `description` longtext DEFAULT 'Đây là mô tả sản phẩm',
+  `created_at` datetime DEFAULT (now()),
+  `updated_at` datetime,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "products" {
-  "product_id" int(11) [unique, pk, not null, increment]
-  "product_name" varchar(350) [not null]
-  "brand_id" int(11) [not null]
-  "categories_id" int(11) [not null]
-  "price" decimal(10,2) [default: 0, note: "Phải >= 0"]
-  "thumbnail" varchar(300) [default: "", note: "Phải có ảnh mặc định"]
-  "description" longtext [default: "Đây là mô tả sản phẩm"]
-  "created_at" datetime [default: `now()`]
-  "updated_at" datetime
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `product_images` (
+  `product_image_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `image_url` varchar(300) DEFAULT '' COMMENT 'Phải có ít nhất 1 ảnh mặc định',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "product_images" {
-  "product_image_id" int(11) [unique, pk, not null, increment]
-  "product_id" int(11) [not null]
-  "image_url" varchar(300) [default: "", note: "Phải có ít nhất 1 ảnh mặc định"]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `reasons` (
+  `reason_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `reason_name` varchar(200) DEFAULT '' COMMENT 'Nhập, Bán, Trả, Khách trả',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "reasons" {
-  "reason_id" int(11) [unique, pk, not null, increment]
-  "reason_name" varchar(200) [default: "", note: "Nhập, Bán, Trả, Khách trả"]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `roles` (
+  `role_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(20) NOT NULL,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "roles" {
-  "role_id" int(11) [unique, pk, not null, increment]
-  "role_name" varchar(20) [not null]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `shipments` (
+  `shipment_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `import_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `unit_price_import` decimal(10,2) DEFAULT 0 COMMENT 'Phải > 0',
+  `quantity` int(50) DEFAULT 0 COMMENT 'Phải > giá trị tối thiểu của 1 lô hàng',
+  `remain` int(50) DEFAULT 0 COMMENT 'Phải bé 1 số lượng cụ thể thì mới nhập thêm lô',
+  `sku_id` int(11) NOT NULL,
+  `mfg` date,
+  `exp` date,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "shipments" {
-  "shipment_id" int(11) [unique, pk, not null, increment]
-  "import_id" int(11) [not null]
-  "product_id" int(11) [not null]
-  "unit_price_import" decimal(10,2) [default: 0, note: "Phải > 0"]
-  "quantity" int(50) [default: 0, note: "Phải > giá trị tối thiểu của 1 lô hàng"]
-  "remain" int(50) [default: 0, note: "Phải bé 1 số lượng cụ thể thì mới nhập thêm lô"]
-  "sku_id" int(11) [not null]
-  "mfg" date 
-  "exp" date
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `skus` (
+  `sku_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `sku_code` varchar(100) DEFAULT '' COMMENT 'Phải đủ số lượng ký tự của 1 sku code, nếu có enum về color thì sẽ dễ quản lý hơn',
+  `product_id` int(11) NOT NULL,
+  `color_of_product` varchar(20) DEFAULT '' COMMENT 'Nên có enums',
+  `weight_of_product` float DEFAULT 0 COMMENT 'Phải > 0',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "skus" {
-  "sku_id" int(11) [unique, pk, not null, increment]
-  "sku_code" varchar(100) [default: "", note: "Phải đủ số lượng ký tự của 1 sku code, nếu có enum về color thì sẽ dễ quản lý hơn"]
-  "product_id" int(11) [not null]
-  "color_of_product" varchar(20) [default: "", note: "Nên có enums"]
-  "weight_of_product" float [default: 0, note: "Phải > 0"]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `staffs` (
+  `staff_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `staff_fullname` varchar(100) NOT NULL,
+  `staff_phone_number` varchar(20) NOT NULL,
+  `staff_email` varchar(200) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `gender` bit(1) DEFAULT 0 COMMENT 'Male: 0, Female: 1',
+  `entry_date` date DEFAULT (now()),
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "staffs" {
-  "staff_id" int(11) [unique, pk, not null, increment]
-  "staff_fullname" varchar(100) [not null]
-  "staff_phone_number" varchar(20) [not null]
-  "staff_email" varchar(200) [not null]
-  "role_id" int(11) [not null]
-  "gender" bit(1) [default: 0, note: "Male: 0, Female: 1"]
-  "entry_date" date [default: `now()`]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `statistics` (
+  `statistic_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `statistic_name` varchar(200) NOT NULL COMMENT 'Dùng các function, trigger, procedure, view,... Để tạo ra các dữ liệu muốn thống kê',
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "statistics" {
-  "statistic_id" int(11) [unique, pk, not null, increment]
-  "statistic_name" varchar(200) [not null, note: "Dùng các function, trigger, procedure, view,... Để tạo ra các dữ liệu muốn thống kê"]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `suppliers` (
+  `supplier_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `supplier_name` varchar(200) NOT NULL,
+  `phone_number_of_supplier` varchar(20) NOT NULL,
+  `address_of_supplier` varchar(200) NOT NULL,
+  `email_of_supplier` varchar(100) NOT NULL,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "suppliers" {
-  "supplier_id" int(11) [unique, pk, not null, increment]
-  "supplier_name" varchar(200) [not null]
-  "phone_number_of_supplier" varchar(20) [not null]
-  "address_of_supplier" varchar(200) [not null]
-  "email_of_supplier" varchar(100) [not null]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `wages` (
+  `wage_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `work_time` float DEFAULT 0,
+  `base_salary` decimal(10,2) DEFAULT 0,
+  `allowance` decimal(10,2) DEFAULT 0,
+  `start_date` date,
+  `end_date` date,
+  `is_active` bit(1) DEFAULT 1
+);
 
-Table "wages" {
-  "wage_id" int(11) [unique, pk, not null, increment]
-  "work_time" float [default: 0]
-  "base_salary" decimal(10,2) [default: 0]
-  "allowance" decimal(10,2) [default: 0]
-  "start_date" date
-  "end_date" date
-  "is_active" bit(1) [default: 1]
-}
-
-Table "wage_details" {
-  "wage_detail_id" int(11) [unique, pk, not null, increment]
-  "wage_id" int(11) [not null]
-  "staff_id" int(11) [not null]
-  "payment_date" date [default: `now()`]
-  "is_active" bit(1) [default: 1]
-}
+CREATE TABLE `wage_details` (
+  `wage_detail_id` int(11) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `wage_id` int(11) NOT NULL,
+  `staff_id` int(11) NOT NULL,
+  `payment_date` date DEFAULT (now()),
+  `is_active` bit(1) DEFAULT 1
+);
