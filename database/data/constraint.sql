73750fb8c579ddd331d9bd0fa281b478eed90f54
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-03-01 23:45:17 +0700

    insert data

--- database/data/constraint.sql
+++ database/data/constraint.sql
@@ -7,12 +7,12 @@ BEGIN
 END; //
 DELIMITER ;
 
--- ACCOUNT
--- Kiểm tra mật khẩu có chứa username không khi insert
+---------------------------- ACCOUNT ----------------------------
+-- Kiểm tra mật khẩu có chứa username không
 DELIMITER //
-CREATE TRIGGER check_password_insert
-BEFORE INSERT ON accounts
-FOR EACH ROW
+CREATE TRIGGER `accounts_check_password_insert` 
+BEFORE INSERT ON `accounts`
+FOR EACH ROW 
 BEGIN
 IF INSTR(LOWER(NEW.password), LOWER(NEW.username)) > 0 THEN
     SIGNAL SQLSTATE '45000'
@@ -20,11 +20,10 @@ IF INSTR(LOWER(NEW.password), LOWER(NEW.username)) > 0 THEN
     END IF;
 END; // 
 DELIMITER ;
--- Kiểm tra mật khẩu có chứa username không khi update
 DELIMITER //
-CREATE TRIGGER check_password_update
-BEFORE UPDATE ON accounts
-FOR EACH ROW
+CREATE TRIGGER `accounts_check_password_update` 
+BEFORE UPDATE ON `accounts`
+FOR EACH ROW 
 BEGIN
 IF INSTR(LOWER(NEW.password), LOWER(NEW.username)) > 0 THEN
     SIGNAL SQLSTATE '45000'
@@ -32,3 +31,50 @@ IF INSTR(LOWER(NEW.password), LOWER(NEW.username)) > 0 THEN
     END IF;
 END; // 
 DELIMITER ;
+
+-- Kiểm tra ký tự đặc biệt của username
+DELIMITER //
+CREATE TRIGGER `accounts_check_username_special_characters_insert` 
+BEFORE INSERT ON `accounts`
+FOR EACH ROW begin
+	if new.username regexp '[^a-zA-z0-9]' then
+    	SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Username must not contain special characters';
+    end if;
+end; //
+DELIMITER ;
+DELIMITER //
+CREATE TRIGGER `accounts_check_username_special_characters_update` 
+BEFORE UPDATE ON `accounts`
+FOR EACH ROW 
+BEGIN
+    IF NEW.username REGEXP '[^a-zA-Z0-9]' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Username must not contain special characters';
+    END IF;
+END; //
+DELIMITER ;
+
+-- Kiểm tra xem ngày tạo tk có trước ngày cập nhật tk không
+DELIMITER //
+CREATE TRIGGER `accounts_created_bigger_updated_insert` 
+BEFORE INSERT ON `accounts`
+FOR EACH ROW 
+BEGIN
+    IF NEW.created_at >= NEW.updated_at THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: created_at must be before updated_at';
+    END IF;
+END; //
+DELIMITER ; 
+DELIMITER //
+CREATE TRIGGER `accounts_created_bigger_updated_update` 
+BEFORE UPDATE ON `accounts`
+FOR EACH ROW 
+BEGIN
+    IF NEW.created_at >= NEW.updated_at THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: created_at must be before updated_at';
+    END IF;
+END; //
+DELIMITER ; 
