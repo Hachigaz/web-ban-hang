dfe562ecfe25815a6983e11ea7ddec406117b100
the bad guy <228.quochuy@gmail.com>
2024-04-06 07:41:15 +0700

    jnoj

--- MVC/Controllers/Home.php
+++ MVC/Controllers/Home.php
@@ -9,7 +9,8 @@
         public function SayHi(){
             $ProductLists = [
                 "Laptop" => $this->productService->getProductByType('Laptop'),
-                "SmartPhone" => $this->productService->getProductByType('Điện thoại')
+                "SmartPhone" => $this->productService->getProductByType('Điện thoại'),
+                "SmartPhone" => $this->productService->getProductByType('Smartwatch')
             ];
             $BannerList = [
                 "Banners"=>glob("Public/img/banners/*")
--- MVC/Views/pages/Catalog/ProductPrint.php
+++ MVC/Views/pages/Catalog/ProductPrint.php
@@ -1,7 +1,11 @@
 <?php foreach($productList as $product): ?>
     <a href="../ProductDescription/?id=<?= $product["product_id"]?>" class="product-item no-style">
         <div class="product-image-wrapper">
+            <?php if($product["thumbnail"]!=""): ?>
             <img class="no-user-select" src="../Public/img/products/<?= $product["thumbnail"] ?>" alt="" srcset="">
+            <?php else: ?>
+            <img class="no-user-select" src="../Public/img/products/_common/no-image.jpg ?>" alt="" srcset="">
+            <?php endif ?>
         </div>
         <div class="product-display-name">
             <?= $product["product_name"] ?>
